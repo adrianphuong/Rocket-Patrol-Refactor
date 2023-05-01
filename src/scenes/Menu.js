10806510e2609af98b3cefb842b206f99d4e9ac4
AdrianPhuong <adrianphuong1@gmail.com>
2023-05-01 16:33:59 -0700

    new

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,46 +7,71 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.image('starfield', './assets/starfield.png');
     }
 
     create() {
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    }
+    // set background image
+    this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+    // add title text
+    let titleConfig = {
+      fontFamily: 'Montserrat',
+      fontSize: '72px',
+      color: '#FFFFFF',
+      align: 'center',
+    };
+    this.add.text(game.config.width/2, game.config.height/2 - 150, 'ROCKET PATROL', titleConfig).setOrigin(0.5);
 
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-          game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer: 60000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start("playScene");    
-        }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-          game.settings = {
-            spaceshipSpeed: 4,
-            gameTimer: 45000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start("playScene");    
-        }
+    // add subtitle text
+    let subtitleConfig = {
+      fontFamily: 'Montserrat',
+      fontSize: '36px',
+      color: '#FFFFFF',
+      align: 'center',
+    };
+    this.add.text(game.config.width/2, game.config.height/2 - 50, 'DEFEND YOUR CITY', subtitleConfig).setOrigin(0.5);
+
+    // add instructions text
+    let instructionsConfig = {
+      fontFamily: 'Montserrat',
+      fontSize: '24px',
+      color: '#FFFFFF',
+      align: 'center',
+    };
+    this.add.text(game.config.width/2, game.config.height/2 + 75, 'Use ←→ arrows to move & (F) to fire', instructionsConfig).setOrigin(0.5);
+
+    // add difficulty selection text
+    let difficultyConfig = {
+      fontFamily: 'Montserrat',
+      fontSize: '24px',
+      color: '#FFFFFF',
+      align: 'center',
+    };
+    this.add.text(game.config.width/2, game.config.height/2 + 150, 'Press ← for Novice or → for Expert', difficultyConfig).setOrigin(0.5);
+
+    // set up input keys
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  }
+
+  update() {
+    
+    // handle input key presses
+    if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+      game.settings = {
+        spaceshipSpeed: 3,
+        gameTimer: 60000    
       }
+      this.sound.play('sfx_select');
+      this.scene.start("playScene");    
+    }
+    if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+      game.settings = {
+        spaceshipSpeed: 4,
+        gameTimer: 45000    
+      }
+      this.sound.play('sfx_select');
+      this.scene.start("playScene");    
+    }
+  }
 }
