c5f85e14e021c1ba7c59062cb3315a6dba21cb84
AdrianPhuong <adrianphuong1@gmail.com>
2023-05-01 16:33:04 -0700

    65

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,3 +1,10 @@
+// Display the time remaining (in seconds) on the screen (10)
+// Create a new title screen (e.g., new artwork, typography, layout) (10)
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
+// Implement the speed increase that happens after 30 seconds in the original game (5)
+// Added parallax scrolling to the menu screen
+// Added new spaceship sprites that move faster and worth more points
+// Curr total: 65/100
 class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
@@ -8,6 +15,7 @@ preload() {
     // load images and spritesheets
     this.load.image('rocket', './assets/rocket.png');
     this.load.image('spaceship', './assets/spaceship.png');
+    this.load.image('fastspaceship', `./assets/spaceship.gif`);
     this.load.image('starfield', './assets/starfield.png');
     this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
 }
@@ -28,7 +36,7 @@ create() {
     this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
     this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
     this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
-    this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+    this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'fastspaceship', 0, 10).setOrigin(0,0);
 
     // set up keyboard input
     keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -47,6 +55,41 @@ create() {
         frameRate: 30
     });
 
+    // create a text object to display the remaining time
+    let timeConfig = {
+        fontFamily: 'Courier',
+        fontSize: '28px',
+        backgroundColor: '#F3B141',
+        color: '#843605',
+        align: 'right',
+        padding: {
+        top: 5,
+        bottom: 5,
+        },
+        fixedWidth: 0
+    };
+    this.timeLeft = this.add.text(game.config.width - borderUISize - borderPadding, borderUISize + borderPadding*2, '', timeConfig).setOrigin(1, 0);
+    
+    // set fast space ship speed
+    this.ship03.moveSpeed += 1;
+    // start the game timer
+    this.initialTime = game.settings.gameTimer / 1000; // convert milliseconds to seconds
+    this.timeElapsed = 0;
+    this.gameTimer = this.time.addEvent({
+        delay: 1000, // 1 second
+        callback: function() {
+        this.timeElapsed++;
+        let timeRemaining = this.initialTime - this.timeElapsed;
+        if (timeRemaining <= 0) {
+            timeRemaining = 0;
+            this.gameTimer.remove(false);
+        }
+        this.timeLeft.text = 'Time: ' + timeRemaining;
+        },
+        callbackScope: this,
+        loop: true
+    });
+  
     // set up score tracking
     this.p1Score = 0;
     let scoreConfig = {
@@ -72,6 +115,13 @@ create() {
     }
 
     update() {
+    // check if 30 seconds have passed
+    if (this.initialTime - this.timeElapsed == 30) {
+        // increase the speed of the spaceships
+        this.ship01.moveSpeed += .05;
+        this.ship02.moveSpeed += .05;
+        this.ship03.moveSpeed += .05;
+    }
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
         }
@@ -111,7 +161,7 @@ create() {
     }
 
     shipExplode(ship) {
-        ship.alpha = 0;                         
+        ship.alpha = 0;
         let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
         boom.anims.play('explode');
         boom.on('animationcomplete', () => {
@@ -119,9 +169,18 @@ create() {
             ship.alpha = 1;
             boom.destroy();
         });
-        this.p1Score += ship.points;
-        this.scoreLeft.text = this.p1Score; 
-        
+        if(ship == this.ship03) { // if fast ship
+            this.p1Score += 40;
+        }
+        else{
+            this.p1Score += ship.points;
+        }
+        this.scoreLeft.text = this.p1Score;
+    
+        // Add extra time for each destroyed ship
+        this.initialTime += 3; // Add 3 seconds to the initial time
+        this.timeLeft.text = 'Time: ' + (this.initialTime - this.timeElapsed); // Update the time left text
+    
         this.sound.play('sfx_explosion');
-      }
     }
+}
